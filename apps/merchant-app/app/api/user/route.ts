f38ab3e63ea9a8f51ae01f52f2fd95be4f70c2bd
unknown <adityaraj715426@gmail.com>
2026-05-11 18:52:12 +0530

    added format

--- apps/merchant-app/app/api/user/route.ts
+++ apps/merchant-app/app/api/user/route.ts
@@ -1,16 +1,8 @@
 import { NextResponse } from "next/server"
-import { PrismaClient } from "@repo/db/client";
 
-const client = new PrismaClient();
 
 export const GET = async () => {
-    await client.user.create({
-        data: {
-            email: "asd",
-            name: "adsads"
-        }
-    })
     return NextResponse.json({
-        message: "hi there"
+        message: "Hii, From merchant-app:api/user/"
     })
 }
--- packages/store/src/hooks/useBalance.ts
+++ packages/store/src/hooks/useBalance.ts
@@ -1,7 +1,7 @@
 import { useRecoilValue } from "recoil"
 import { balanceAtom } from "../atoms/balance"
 
-export const useBalance = () => {
+export default  function useBalance(){
     const value = useRecoilValue(balanceAtom);
     return value;
 }
